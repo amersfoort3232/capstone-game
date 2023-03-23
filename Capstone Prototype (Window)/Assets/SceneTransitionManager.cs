fcfa59ba42f6445d5cd30e37309ff20c34efb1f3
amersfoort3232 <amersfoortlam@gmail.com>
2023-03-23 11:27:53 +0800

    23/3/2023

--- Capstone Prototype (Window)/Assets/SceneTransitionManager.cs
+++ Capstone Prototype (Window)/Assets/SceneTransitionManager.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneTransitionManager : MonoBehaviour
 {
     public int sceneIndex;
-    public void OnTriggerEnter (Collider other) {
-        if (other.CompareTag("Player")) {
+
+
+    void OnTriggerEnter (Collider other) {
+        if(other.CompareTag("Player")) {
             SceneManager.LoadScene(sceneIndex);
-    }          
         }
-        
+    }
 }
